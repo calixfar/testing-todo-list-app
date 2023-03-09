61a0adcc5f2861244827e17aea895db66022768c
luissantiago <luis.santiago@itglobers.com>
2023-03-09 15:34:34 -0500

    refactor: events callbacks isolated in their own function

--- src/views/todo/todo.tsx
+++ src/views/todo/todo.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
+interface Item {
+  value: string, isDone: boolean
+}
 interface Props {
-  items: {value: string, isDone: boolean}[]
+  items: Item[]
 }
 
 export const Todo = ({ items }: Props) => {
   const [value, setValue] = React.useState('')
   const [todoItems, setTodoItems] = React.useState(items)
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && value) {
+      setTodoItems((oldTodoItems) => [...oldTodoItems, {value, isDone: false}])
+      setValue('')
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => setValue(() => event.target.value)
+
+  function handleCheckBoxChange (this: {item: Item}) {
+    setTodoItems((oldTodoItems) => oldTodoItems.map((todoItem) => todoItem.value === this.item.value ? {...todoItem, isDone: !todoItem.isDone} : todoItem))
+  }
 
   return (
     <div>
@@ -14,18 +30,12 @@ export const Todo = ({ items }: Props) => {
         <input 
           type="text" 
           value={value} 
-          onChange={(event) => setValue(() => event.target.value)}
-          onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-            if (event.key === 'Enter' && value) {
-              setTodoItems((oldTodoItems) => [...oldTodoItems, {value, isDone: false}])
-              setValue('')
-            }
-          }}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
         />
       </div>
       <div>
         {todoItems.map((item) => {
-          
           const getCustomId = (section: string) => `${section}-${item.value.replace(/ /g, '-')}`
           const customCheckBoxId = getCustomId('checkbox')
 
@@ -40,9 +50,7 @@ export const Todo = ({ items }: Props) => {
                 id={customCheckBoxId}
                 data-testid={customCheckBoxId}
                 defaultChecked={item.isDone}
-                onChange={() => {
-                  setTodoItems((oldTodoItems) => oldTodoItems.map((todoItem) => todoItem.value === item.value ? {...todoItem, isDone: !todoItem.isDone} : todoItem))
-                }}
+                onChange={handleCheckBoxChange.bind({item})}
               />
               <label htmlFor={customCheckBoxId}>{item.value}</label>
             </div>
